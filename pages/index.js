65d5286e6ddde6bc2ac008a1535e6bb5bc15ea3e
hasnatuz-zahara <mhasnatuz@gmail.com>
2024-08-01 11:11:08 +0600

    create sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Sidebar from "@/components/Sidebar";
 import Head from "next/head";
 
 export default function Home() {
@@ -9,6 +10,21 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+
+      <main className="flex min-h-screen max-w-7xl mx-auto">
+
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Feed */}
+
+        {/* Widgets */}
+
+        {/* Modal */}
+
+      </main>
+
     </div>
   );
 }
